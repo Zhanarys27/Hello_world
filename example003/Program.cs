597c83571a33034c6f940fba731c4c0af8bad322
ZHANARYS <nazarbakzhanarys@gmail.com>
2023-03-03 01:11:35 +0600

    add example 003 Random

--- example003/Program.cs
+++ example003/Program.cs
@@ -1,6 +1,13 @@
-﻿Console.WriteLine("Введите первое число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int b = Convert.ToInt32(Console.ReadLine());
-int Sum = a + b;
-Console.WriteLine("Ваша сумма " + Sum);
+﻿
+// Console.WriteLine("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int Sum = a + b;
+// Console.WriteLine("Ваша сумма " + Sum);
+
+int a = new Random().Next(1,10);
+Console.WriteLine(a);
+int b = new Random().Next(1,10);
+Console.WriteLine(b);
+Console.WriteLine(a+b);
