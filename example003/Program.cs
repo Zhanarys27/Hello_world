94ad5bab72b345f507a0731df049bf22f9482e31
ZHANARYS <nazarbakzhanarys@gmail.com>
2023-03-03 00:43:14 +0600

    add example 003 summ numbers

---
+++ example003/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int Sum = a + b;
+Console.WriteLine("Ваша сумма " + Sum);
