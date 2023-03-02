787f31c949982deb0f8ca6b3e74797a0274cfdd3
ZHANARYS <nazarbakzhanarys@gmail.com>
2023-03-03 01:33:11 +0600

    add example 004 Meet user

---
+++ Example004/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Write your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Zhan")
+{
+    Console.Write("Wow, it`s Zhan");
+}
+else
+{
+    Console.Write("Hello, "+ username);
+}
